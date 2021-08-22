a3f3756b0a490eca6888ca07e522c44f9c490cc0
kristina100 <81134471@qq.com>
2021-08-22 20:37:19 +0800

    renew

--- README.md
+++
@@ -1,4 +0,0 @@
-# BigDataAnalysisSystemforCabs
-
-:oncoming_taxi:QG最终考核： 基于差分隐私和分布式聚类方法的 出租车大数据分析系统
-
--- cabs-analysis/src/components/MyNavLink/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-
-function MyNavLink(props) {
-	return (
-		<NavLink activeClassName="onActive" className="noActive" {...props} />
-	)
-}
-export default MyNavLink;
